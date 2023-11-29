2613aa621c2016d1e6ee0081cf8df09522ef7893
Julia Staniszewska <staniszewska.julia@wp.pl>
2023-11-29 13:54:52 +0000

    logo

---
+++ front/src/views/ConnectedView.vue
@@ -0,0 +1,8 @@
+<template>
+<div class="box">
+<div class="text">
+ Trwa połączenie...
+
+</div>
+</div>
+</template>
--- front/vue.config.js
+++ front/vue.config.js
@@ -1,10 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true,
- devServer: {
- historyApiFallback: true,
- allowedHosts: "all",
- host: 'localhost',
- port: 8080,
+  devServer: {
+    historyApiFallback: true,
+    allowedHosts: "all",
+    host: 'localhost',
+    port: 8080,
  },
 });
